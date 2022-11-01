d7d015ecc4c7751e60f6247f011ea80c514a11c6
flirtwithcode <109421967+flirtwithcode@users.noreply.github.com>
2022-11-01 08:42:56 +0530

    1 file modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule , LoginModule],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
